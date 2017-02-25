e291ad62740e3b962180e03482f460a7658d4f77
Andy <andy9775@gmail.com>
2017-02-24 21:43:56 -0500

    chapter 5 - models

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -6,7 +6,44 @@ class QuotesController < Rulers::Controller
     render :a_quote, noun: 'winking'
   end
 
+	def quote_1
+		quote_1 = FileModel.find 1
+		render :quote, obj: quote_1
+	end
+
   def exception
     raise 'Something happened'
+  end
+
+  def index
+    quotes = FileModel.all
+    render :index, quotes: quotes
+  end
+
+  def new_quote
+    attrs = {
+      "submitter": "A Web User",
+      "quote": "A picture is worth one k pixels",
+      "attribution": "me"
+    }
+
+    m = FileModel.create attrs
+    render :quote, obj: m
+  end
+
+  def update
+    if env['REQUEST_METHOD'] == 'POST'
+      quote_2 = FileModel.find 2
+      quote_2["submitter"] = 'Me'
+      quote_2.save
+      render :quote, obj: quote_2
+    else
+      raise "Invalid method. Expected POST got #{env['REQUEST_METHOD']}"
+    end
+  end
+
+  def by_submitter
+    submissions = FileModel.find_all_by_submitter('me')
+    render :index, quotes: submissions
   end
 end
